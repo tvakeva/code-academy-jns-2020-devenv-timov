7e52e453612e760e57f725e70d0566b315f29811
Antti Vikman <antti.vikman@nolwenture.com>
2020-06-09 11:10:11 +0300

    Add form state handling using react hooks

--- client/src/containers/FormSamples/FormWithHooks.js
+++ client/src/containers/FormSamples/FormWithHooks.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Form, FormGroup, Label, Input, Row, Col } from "reactstrap";
+import React, { useState } from "react";
+import { Form, FormGroup, Label, Input, Row, Col, Button } from "reactstrap";
 import {
   FaUser,
   FaEnvelope,
@@ -8,8 +8,20 @@ import {
 } from "react-icons/fa";
 
 function FormWithHooks(props) {
+  const [formData, setFormData] = useState({ cardType: "visa" });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
+  const onChange = (event) => {
+    event.preventDefault();
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <Row>
         <Col>
           <h3>Billing Address</h3>
@@ -17,7 +29,13 @@ function FormWithHooks(props) {
             <Label for="fullname">
               <FaUser /> Full Name
             </Label>
-            <Input type="text" name="fullname" placeholder="Maisa Mallikas" />
+            <Input
+              type="text"
+              name="fullname"
+              placeholder="Maisa Mallikas"
+              value={formData.fullname || ""}
+              onChange={onChange}
+            />
           </FormGroup>
           <FormGroup>
             <Label for="email">
@@ -27,13 +45,21 @@ function FormWithHooks(props) {
               type="text"
               name="email"
               placeholder="maisa.mallikas@example.com"
+              value={formData.email || ""}
+              onChange={onChange}
             />
           </FormGroup>
           <FormGroup>
             <Label for="address">
               <FaRegAddressCard /> Address
             </Label>
-            <Input type="text" name="address" placeholder="Länsikatu 15" />
+            <Input
+              type="text"
+              name="address"
+              placeholder="Länsikatu 15"
+              value={formData.address || ""}
+              onChange={onChange}
+            />
           </FormGroup>
           <Row>
             <Col>
@@ -41,13 +67,25 @@ function FormWithHooks(props) {
                 <Label for="city">
                   <FaUniversity /> City
                 </Label>
-                <Input type="text" name="city" placeholder="Joensuu" />
+                <Input
+                  type="text"
+                  name="city"
+                  placeholder="Joensuu"
+                  value={formData.city || ""}
+                  onChange={onChange}
+                />
               </FormGroup>
             </Col>
             <Col>
               <FormGroup>
                 <Label for="zip">Zip</Label>
-                <Input type="text" name="zip" placeholder="80110" />
+                <Input
+                  type="text"
+                  name="zip"
+                  placeholder="80110"
+                  value={formData.zip || ""}
+                  onChange={onChange}
+                />
               </FormGroup>
             </Col>
           </Row>
@@ -56,14 +94,25 @@ function FormWithHooks(props) {
           <h3>Payment</h3>
           <FormGroup>
             <Label for="cardType">Select Card</Label>
-            <Input type="select" name="cardType">
+            <Input
+              type="select"
+              name="cardType"
+              onChange={onChange}
+              value={formData.cardType || ""}
+            >
               <option value="visa">Visa</option>
               <option value="masterCard">Mastercard</option>
             </Input>
           </FormGroup>
           <FormGroup>
             <Label for="cardName">Name on Card</Label>
-            <Input type="text" name="cardName" placeholder="Maisa Mallikas" />
+            <Input
+              type="text"
+              name="cardName"
+              placeholder="Maisa Mallikas"
+              value={formData.cardName || ""}
+              onChange={onChange}
+            />
           </FormGroup>
           <FormGroup>
             <Label for="cardNumber">Credit card number</Label>
@@ -71,30 +120,51 @@ function FormWithHooks(props) {
               type="text"
               name="cardNumber"
               placeholder="1111-2222-3333-4444"
+              value={formData.cardNumber || ""}
+              onChange={onChange}
             />
           </FormGroup>
           <Row>
             <Col>
               <FormGroup>
                 <Label for="cardExpMonth">Exp Month</Label>
-                <Input type="number" name="cardExpMonth" placeholder="06" />
+                <Input
+                  type="number"
+                  name="cardExpMonth"
+                  placeholder="06"
+                  value={formData.cardExpMonth || ""}
+                  onChange={onChange}
+                />
               </FormGroup>
             </Col>
             <Col>
               <FormGroup>
                 <Label for="cardExpYear">Exp Year</Label>
-                <Input type="number" name="cardExpYear" placeholder="2020" />
+                <Input
+                  type="number"
+                  name="cardExpYear"
+                  placeholder="2020"
+                  value={formData.cardExpYear || ""}
+                  onChange={onChange}
+                />
               </FormGroup>
             </Col>
             <Col>
               <FormGroup>
                 <Label for="cardCvv">CVV</Label>
-                <Input type="text" name="cardCvv" placeholder="123" />
+                <Input
+                  type="text"
+                  name="cardCvv"
+                  placeholder="123"
+                  value={formData.cardCvv || ""}
+                  onChange={onChange}
+                />
               </FormGroup>
             </Col>
           </Row>
         </Col>
       </Row>
+      <Button type="submit">Send</Button>
     </Form>
   );
 }
