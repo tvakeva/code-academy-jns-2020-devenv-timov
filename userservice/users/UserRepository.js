f6f962880023569840fc1d8ef7b5f0f1f296da35
Antti Vikman <antti.vikman@nolwenture.com>
2020-07-03 13:52:47 +0300

    move user to use share database connection

--- userservice/users/UserRepository.js
+++ userservice/users/UserRepository.js
@@ -1,42 +1,27 @@
-const { MongoClient, ObjectId } = require("mongodb");
-const mongoDbUrl = "mongodb://root:example@mongo:27017/";
+const { ObjectId } = require("mongodb");
+const { getCollection } = require("../db");
 
 const addUser = (newUser, callback) => {
-  MongoClient.connect(mongoDbUrl, (err, dbconn) => {
+  const collection = getCollection("userservice", "users");
+  collection.insertOne(newUser, (err, dbRes) => {
     if (err) throw err;
-    const dbo = dbconn.db("userservice");
-    const collection = dbo.collection("users");
-    collection.insertOne(newUser, (err, dbRes) => {
-      if (err) throw err;
-      callback(dbRes.ops[0]);
-      dbconn.close();
-    });
+    callback(dbRes.ops[0]);
   });
 };
 const getUsers = (callback) => {
-  MongoClient.connect(mongoDbUrl, (err, dbconn) => {
+  const collection = getCollection("userservice", "users");
+  collection.find({}).toArray((err, result) => {
     if (err) throw err;
-    const dbo = dbconn.db("userservice");
-    const collection = dbo.collection("users");
-    collection.find({}).toArray((err, result) => {
-      if (err) throw err;
-      callback(result);
-      dbconn.close();
-    });
+    callback(result);
   });
 };
 const removeUser = (userId, callback) => {
-  MongoClient.connect(mongoDbUrl, (err, dbconn) => {
-    if (err) throw err;
-    const dbo = dbconn.db("userservice");
-    const collection = dbo.collection("users");
+  const collection = getCollection("userservice", "users");
 
-    const deleteQuery = { _id: ObjectId(userId) };
-    collection.deleteOne(deleteQuery, (err, dbRes) => {
-      if (err) throw err;
-      callback();
-      dbconn.close();
-    });
+  const deleteQuery = { _id: ObjectId(userId) };
+  collection.deleteOne(deleteQuery, (err, dbRes) => {
+    if (err) throw err;
+    callback();
   });
 };
 
