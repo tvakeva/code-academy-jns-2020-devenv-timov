368cb87dc8aaa48154d2467df3f613b5c22fe5d8
Antti Vikman <antti.vikman@nolwenture.com>
2020-07-06 19:25:53 +0300

    Adding the Book model that uses PostgreSQL database for storing the data
    (cherry picked from commit abd5984b881aa1a28fbce1e2f44ec71c6e470f97)

---
+++ libraryservice/src/main/java/com/example/demo/Book.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+    private String title;
+    private Integer publicationYear;
+
+    Book() {
+    }
+
+    Book(String title, Integer publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getPublicationYear() {
+        return publicationYear;
+    }
+
+    public String toString() {
+        return "" + id + ": " + title + "(" + publicationYear + ")";
+    }
+}
