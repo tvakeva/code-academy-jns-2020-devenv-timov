abd5984b881aa1a28fbce1e2f44ec71c6e470f97
Antti Vikman <antti.vikman@nolwenture.com>
2020-07-06 19:25:53 +0300

    Adding the Book model that uses PostgreSQL database for storing the data

---
+++ libraryservice/src/main/java/com/example/demo/BooksController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksController {
+    private final BookRepository repository;
+
+    BooksController(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/books")
+    public List<Book> getBooks() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/books")
+    public Book addBook(@RequestBody Book newBook) {
+        return repository.save(newBook);
+    }
+
+    @DeleteMapping("/books/{bookId}")
+    void deleteBook(@PathVariable Long bookId) {
+        repository.deleteById(bookId);
+    }
+}
