2a77506943f1f24fed0a5c4de22375ed3f638a88
Antti Vikman <antti.vikman@nolwenture.com>
2020-07-07 09:48:13 +0300

    Make DB persisting data between boots and refactor code placements

---
+++ libraryservice/src/main/java/com/example/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package com.example.repositories;
+
+import com.example.models.Book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
